17301e58e60e428bde360651fc56d6ce049908e4
Arunmudiraaj <arunmudiraj18@gmail.com>
2022-12-26 12:43:06 +0530

    second

--- task4.js
+++ task4.js
@@ -1,1 +1,9 @@
-document.getElementsByTagName('li')[4].style.color = "red"
+// document.getElementsByTagName('li')[4].style.color = "red"
+// // using query selector
+
+// document.querySelector('.list-group-item:nthchild(2)').style.background = 'green'
+// document.querySelector('.list-group-item:nthchild(3)').style.display = 'none'
+var odditems = document.querySelectorAll('li:nth-child(odd)')
+for(var i=0;i<odditems.length;i++){
+    odditems[i].style.background = 'green'
+}
